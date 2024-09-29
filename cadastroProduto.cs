11ffa5f74992e6a23c07c3554504c8ff6af97376
gabriele teodoro <gabisouzat8@gmail.com>
2024-09-29 12:46:05 -0300

    subindo arquivo

---
+++ cadastroProduto.cs
@@ -0,0 +1,16 @@
+
+using System;
+using System.Linq;
+
+namespace cadastroProduto
+{
+    public static class Program
+    {
+        public static void Main(){
+            
+            Console.WriteLine("Olá"); // Use Console.WriteLine para exibir a mensagem
+            Console.ReadLine();
+
+        }
+    }
+}
